da75c463d4b16b1f6144043129a402445f6114ce
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-09-18 10:45:26 +0600

    add simelar in data simelar in look

--- src/App.js
+++ src/App.js
@@ -2,36 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const myStyle = {
-    background: 'mediumvioletred',
-    padding: '10px',
-    border:' 5px solid mediumpurple',
-    borderRadius: '5px',
-  }
+ 
   return (
     <div className="App">
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
       
-      <header className="App-header">
-        <h1 className="my-header">yo yo React babu!!</h1>
-        <p style={myStyle}>Hello, React mona How are you?</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a style ={{
-    background: 'mediumvioletred',
-    padding: '10px',
-    border:' 5px solid mediumpurple',
-    borderRadius: '5px',
-  }}
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React for me
-        </a>
-      </header>
+    </div>
+  );
+}
+
+function Person() {
+  return (
+    <div className="person">
+      <h1>Abu Daud Hossain Sumon</h1>
+      <p>Full Stack Devloper</p>
     </div>
   );
 }
