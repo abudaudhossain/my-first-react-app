21c4611eb8b3dfc953be8dfd9d276f17b0d2d079
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-09-18 11:50:07 +0600

    make danimik app

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ const myFriend = [{name: "Abu Daud Hossain", job: "Web Devloper"},{name:"Raule Mia", job:"Techer"},{name:"Sakib al Hassan", job:"Criketer"},{name: "Abu Sayd", job: "Software Engin"}]
+//  console.log(myFriend);
  
   return (
     <div className="App">
-      <Person name="Abu Daud Hossain" job="Web Devloper"></Person>
-      <Person name="Raule Mia" job="Techer"></Person>
-      <Person name="Rahes Kha"></Person>
-      <Person name="Sakib al Hassan" job="Criketer"></Person>
-      <Person name="Abu Daud Hossain" job="Web Devloper"></Person>
-      <Person name="Raule Mia" job="Techer"></Person>
-      <Person name="Rahes Kha"></Person>
-      <Person name="Sakib al Hassan" job="Criketer"></Person>
       
+    <ul>
+     {
+        myFriend.map(friend =><li>{friend.name}{friend.job}</li>)
+     }
+    </ul>
+      {/* <Person name="Abu Daud Hossain" job="Web Devloper"></Person>
+      <Person name="Raule Mia" job="Techer"></Person>
+      <Person name="Rahes Kha"></Person>
+      <Person name="Sakib al Hassan" job="Criketer"></Person>
+      <Person name="Abu Daud Hossain" job="Web Devloper"></Person>
+      <Person name="Raule Mia" job="Techer"></Person>
+      <Person name="Rahes Kha"></Person>
+      <Person name="Sakib al Hassan" job="Criketer"></Person> */}
+      
+      {
+        myFriend.map(friend =>  <Person name={friend.name} job={friend.job}></Person>)
+      }
     </div>
   );
 }
 
 function Person(props) {
-  console.log(props);
+  // console.log(props);
   return (
     <div className="person">
       <h1>{props.name}</h1>
